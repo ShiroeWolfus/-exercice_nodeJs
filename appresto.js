2e97ecce9c8c64ff36ef2507b3607c75c2b1914e
ShiroeWolfus <120398665+ShiroeWolfus@users.noreply.github.com>
2023-02-06 15:54:23 +0100

    Add files via upload

--- appresto.js
+++ appresto.js
@@ -1,19 +1,16 @@
-// Les variables constantes relatives aux librairies installées suite au npm init à la racine du fichier pour créer
-// les modules. On y ajoute express et fs. Nodemon est en sauvegarde pour le fichier final
+//constante pour les framework de construction du serveur(express) et lecture des fichier (fs)
 const express = require('express');
 const fs = require('fs');
-const app = express();
 
-// Commentaire du fichier resto-menu : Je nomme le tableau menu et j'y ajoute la ref du plat, son nom ainsi que son prix. Chaque 
-    // index du tableau doit être séparé par une virgule sauf le dernier
+const app = express();
 
 //on indique le chemin du fichier que va récupérer le GET 
 app.get('/menu', (requet, response)=> {
-    //Je demande à FS d'importer les données du JSON que j'ai crée et d'afficher un message d'erreur si la lecture ne s'effectue pas
+    //Je demande à FS d'importer les données du JSON que j'ai crée 
     fs.readFile('menu.json', (err, menu)=> {
+        //si la récupération n'a pas lieu, la propriété statusCode renvoie une erreur
         if (err){
             response.status(500).json({
-                //si le GET ne passe pas sur le fichier. On affiche ce message sur la console Thunder request
                 message: "Une erreur est survenue lors de la lecture des données du menu",
                 error: err
             })
@@ -27,8 +24,3 @@ app.get('/menu', (requet, response)=> {
 app.listen(3000, ()=> {
     console.log("l'application tourne sur le port Andre3000... Hey Ya !");
 })
-
-//dans le fichier package.json on ajoute la ligne     "start": "nodemon appresto.js" sous la balise script pour lancer la lecture du fichier demandé comportant la fonction fléché ligne 11
-//je mets mes commentaires ici car j'avais une erreur sur les fichier .json.
-//après avoir lancé le fichier via le terminal pour simuler la relation client serveur, je lance une requête sur Thunder Client pour afficher le JSON avec les plats et prix ainsi que leur id
-//la commande http://localhost:3000/menu pour afficher le JSON 
